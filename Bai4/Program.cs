09c3fff95daf2c5ea3171f7917c4f49aab1d1250
charleshuynguyen <huynq.charles@gmail.com>
2024-01-19 11:04:04 +0700

    first commit

---
+++ Bai4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                SanPham sanPham = new SanPham();
+                Console.Write($"Ma SP {i}: ");
+                sanPham.MaSP = int.Parse(Console.ReadLine());
+                Console.Write($"Ten SP {i}: ");
+                sanPham.TenSP = Console.ReadLine();
+                Console.Write($"Don Gia SP {i}: ");
+                sanPham.DonGia = double.Parse(Console.ReadLine());
+                Console.Write($"So Luong SP {i}: ");
+                sanPham.SoLuong = int.Parse(Console.ReadLine());
+
+                arrayList.Add(sanPham);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----------------------------");
+
+            foreach (SanPham sanPham in arrayList)
+            {
+                sanPham.In();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bai4/SanPham.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    internal class SanPham
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public double DonGia { get; set; }
+        public int SoLuong { get; set; }
+
+        public SanPham()
+        {
+            
+        }
+
+        public SanPham(int maSP, string tenSP, double donGia, int soLuong)
+        {
+            MaSP = maSP;
+            TenSP = tenSP;
+            DonGia = donGia;
+            SoLuong = soLuong;
+        }
+
+        public double ThanhTien()
+        {
+            return DonGia * SoLuong;
+        }
+
+        public void In()
+        {
+            Console.WriteLine("Ma SP: " + this.MaSP);
+            Console.WriteLine("Ten SP: " + this.TenSP);
+            Console.WriteLine("Don Gia: " + this.DonGia);
+            Console.WriteLine("So Luong: " + this.SoLuong);
+            Console.WriteLine("Thanh Tien: " + ThanhTien());
+        }
+    }
+}
